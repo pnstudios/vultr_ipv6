ae699e9ed2ff80db5cb84e7ca2ea509037c2e64a
Phuoc Nguyen <66317627+pnstudios@users.noreply.github.com>
2024-04-23 18:06:58 +0700

    Update vultr_ipv6_pnteam.sh

--- vultr_ipv6_pnteam.sh
+++ vultr_ipv6_pnteam.sh
@@ -82,7 +82,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "pnteam/pnteam123/$IP4/$port/$(gen64 $IP6)"
+        echo "pnteam/pnteam@@2024/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
