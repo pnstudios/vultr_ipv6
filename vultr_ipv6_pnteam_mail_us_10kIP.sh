875ca45e89c856c6025d04efd424b83da8f4ae42
Phuoc Nguyen <66317627+pnstudios@users.noreply.github.com>
2025-06-12 03:33:29 +0700

    Update vultr_ipv6_pnteam_mail_us_2kIP.sh

--- vultr_ipv6_pnteam_mail_us_10kIP.sh
+++ vultr_ipv6_pnteam_mail_us_10kIP.sh
@@ -113,7 +113,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=50001
-LAST_PORT=60001
+LAST_PORT=52001
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
